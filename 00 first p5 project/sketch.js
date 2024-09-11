773e1a07cd188a890c89708e470dd9cf962f2179
4601822 <devinhubbard214@gmail.com>
2024-09-11 11:26:41 -0600

    started demo 01

---
+++ 00 first p5 project/sketch.js
@@ -0,0 +1,17 @@
+// First Day Demo
+// Devin Hubbard
+// Sept 10, 2024
+//
+// Interactive programs -> dont finish...
+
+
+function setup() {
+  // happens one time at the start
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // draw happens over and over, targets 60fps
+  //background(220);
+  circle(mouseX,mouseY,20);
+}
