c3948ea2b08e2d6366c690b042e335e4a2d7b070
4601822 <devinhubbard214@gmail.com>
2024-09-11 12:15:34 -0600

    Update sketch.js

--- 01 Basics of drawing/sketch.js
+++ 01 Basics of drawing/sketch.js
@@ -1,15 +1,26 @@
-// Project Title
-// Your Name
-// Date
+// 01 Basics Of Drawing
+// Devin Hubbard
+// September 11, 2024
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// 2D primitives, color, animation, mouse/keyboard events
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
   background(220);
+  fill(50,70,100)
+  circle(0,0,50);
+  fill(150,255,90);
+  circle(width,0,50);
+  fill(210,40,0)
+  circle(0,height,50)
+  fill(0,0,220)
+  circle(width,height,50)
+  fill(120,120,50)
+  circle(width/2,height/2,50)
+  fill(150,150,150)
+  circle(mouseX,mouseY,20)
+  circle(width/3*2,height/3*2,50)
 }
