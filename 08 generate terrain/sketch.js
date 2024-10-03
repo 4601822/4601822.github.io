d779e8c9e4bd7a7df2a4a12cd971d766419cbc1e
4601822 <devinhubbard214@gmail.com>
2024-10-03 11:17:57 -0600

    flag is functional

--- 08 generate terrain/sketch.js
+++ 08 generate terrain/sketch.js
@@ -9,17 +9,19 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(225);
-
+  terrain();
 }
 let rectWidth = 10;
 let time = 5
 let flag_Y = 30000
+let flag_top = 30
 function draw() {
   if (keyIsDown(LEFT_ARROW) === true) {
     if (rectWidth >= 0.2){
      rectWidth -= 0.1;
      background(220);
      terrain();
+     flag();
   }
 
   }
@@ -27,11 +29,13 @@ function draw() {
     rectWidth += 0.1;
     background(220);
     terrain();
+    flag();
   }
 }
 
 function terrain(){
   flag_Y = 0
+  flag_X = 0
   for(let x = 0; x <= width; x += rectWidth){
     noFill;
     time += 0.02
@@ -42,13 +46,25 @@ function terrain(){
     //flag position
     if (rectHeight > flag_Y){
       flag_Y = rectHeight;
-      strokeWeight(5);
-stroke(0,255,0);
-      line(x,height-flag_Y,x,height-(flag_Y-20));
-      strokeWeight(1);
-      stroke(0);
-
+      flag_X = x
     }
-    } 
- }
+  } 
+}
 
+function flag(){
+  strokeWeight(3);
+  flag_top = height-flag_Y-30;
+  fill(255,0,0);
+  line(flag_X,height-flag_Y,flag_X,flag_top);
+  triangle(flag_X,flag_top,flag_X,flag_top+15,flag_X+15,flag_top+7.5);
+  noFill();
+  strokeWeight(1);
+}
+
+
+
+//  strokeWeight(5);
+//  stroke(0,255,0);
+//        line(x,height-flag_Y,x,height-(flag_Y-20));
+//        strokeWeight(1);
+//        stroke(0)
